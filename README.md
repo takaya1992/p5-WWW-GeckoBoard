0895edbace60832a212e8b8e308db16ab8c284c3
Wataru Abe <wataru.abe@mixi.co.jp>
2014-03-17 16:43:30 +0900

    initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WWW::GeckoBoard - It's new $module
+
+# SYNOPSIS
+
+    use WWW::GeckoBoard;
+
+# DESCRIPTION
+
+WWW::GeckoBoard is ...
+
+# LICENSE
+
+Copyright (C) Wataru Abe.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Wataru Abe <wataru.abe@mixi.co.jp>
---
+++ lib/WWW/GeckoBoard.pm
@@ -0,0 +1,39 @@
+package WWW::GeckoBoard;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WWW::GeckoBoard - It's new $module
+
+=head1 SYNOPSIS
+
+    use WWW::GeckoBoard;
+
+=head1 DESCRIPTION
+
+WWW::GeckoBoard is ...
+
+=head1 LICENSE
+
+Copyright (C) Wataru Abe.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Wataru Abe E<lt>wataru.abe@mixi.co.jpE<gt>
+
+=cut
+
